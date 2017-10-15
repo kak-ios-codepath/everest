0b64706c0a6fc998f10d379b05653a0ab71038cb
Kaushik <urskaush@gmail.com>
2017-10-14 20:15:58 -0700

    Created service manager and timeline manager classes.

---
+++ Everest/Everest/Controllers/TimeLine/TimeLineManager.swift
@@ -0,0 +1,13 @@
+//
+//  TimeLineManager.swift
+//  Everest
+//
+//  Created by Kaushik on 10/14/17.
+//  Copyright © 2017 Kavita Gaitonde. All rights reserved.
+//
+
+import UIKit
+
+class TimeLineManager: NSObject {
+
+}
---
+++ Everest/Everest/Views/ServiceManager.swift
@@ -0,0 +1,21 @@
+//
+//  ServiceManager.swift
+//  Everest
+//
+//  Created by Kaushik on 10/14/17.
+//  Copyright © 2017 Kavita Gaitonde. All rights reserved.
+//
+
+
+
+/*
+ 
+ This class is a global manager class which is responsible for handling any network requests or responses. 
+ For now we can use this class to talk to firebase and fetch the data.
+ */
+
+import UIKit
+
+class ServiceManager: NSObject {
+
+}
