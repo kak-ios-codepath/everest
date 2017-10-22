6dc2598ece9924a7230bdd1bae9884ec711f1d5d
Kaushik <urskaush@gmail.com>
2017-10-22 13:45:03 -0700

    Resolving the missing extension file issue.

---
+++ Everest/Everest/Extensions/Array+Extension.swift
@@ -0,0 +1,13 @@
+//
+//  Array+Extension.swift
+//  Everest
+//
+//  Created by Kaushik on 10/22/17.
+//  Copyright © 2017 Kavita Gaitonde. All rights reserved.
+//
+
+import UIKit
+
+class Array_Extension: NSObject {
+
+}
