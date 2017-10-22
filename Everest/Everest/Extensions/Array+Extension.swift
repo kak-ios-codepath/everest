417ecc464172e8b5f2783f206053737c68a86a38
Kaushik <urskaush@gmail.com>
2017-10-22 13:46:00 -0700

    Resolving missing extensions issue.

--- Everest/Everest/Extensions/Array+Extension.swift
+++ Everest/Everest/Extensions/Array+Extension.swift
@@ -2,12 +2,21 @@
 //  Array+Extension.swift
 //  Everest
 //
-//  Created by Kaushik on 10/22/17.
+//  Created by Kavita Gaitonde on 10/22/17.
 //  Copyright © 2017 Kavita Gaitonde. All rights reserved.
 //
 
-import UIKit
+import Foundation
 
-class Array_Extension: NSObject {
-
+extension Array {
+    
+    func toDictionary<Key: Hashable>(with selectKey: (Element) -> Key) -> [Key:Element] {
+        var dict = [Key:Element]()
+        for element in self {
+            dict[selectKey(element)] = element
+        }
+        return dict
+    }
+    
+    
 }
