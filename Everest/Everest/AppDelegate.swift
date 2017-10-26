9ccb9fba1f1f390a5404a568b594327815272467
Akrm Almsaodi <aalmsaodi@gmail.com>
2017-10-25 17:40:14 -0700

    Main Act Notification is Implemented

--- Everest/Everest/AppDelegate.swift
+++ Everest/Everest/AppDelegate.swift
@@ -13,20 +13,24 @@ import UserNotifications
 import FirebaseMessaging
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
-
+class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterDelegate {
+    
     var window: UIWindow?
+    let centerNotification = UNUserNotificationCenter.current()
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-
+        
         //setup notification observers
         NotificationCenter.default.addObserver(forName: NSNotification.Name(rawValue: "UserLoggedOut"), object: nil, queue: OperationQueue.main, using: {(Notification) -> () in
             //TODO: perform any needed cleanup here
             self.switchToLoginController()
         })
-
+        
         registerForPushNotifications()
-        getNotificationSettings()
+        if let actionNotificationsData = UserDefaults.standard.object(forKey: "actionNotifications") as? NSData {
+            let actionNotifications = NSKeyedUnarchiver.unarchiveObject(with: actionNotificationsData as Data) as? [Act]
+            SettingsViewController.actionNotifications = actionNotifications!
+        }
         
         //Firebase setup
         FirebaseApp.configure()
@@ -34,13 +38,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         //Facebook setup
         SDKApplicationDelegate.shared.application(application, didFinishLaunchingWithOptions: launchOptions)
         
-        
         //App setup
         MainManager.shared.initialize()
-                
+        
         return true
     }
-
+    
     func switchToLoginController() {
         let storyboard = UIStoryboard(name: "Login", bundle: nil)
         let vc = storyboard.instantiateViewController(withIdentifier: "LoginViewController")
@@ -51,12 +54,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
     }
-
+    
     func applicationDidEnterBackground(_ application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
     }
-  
+    
     func applicationWillEnterForeground(_ application: UIApplication) {
         // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
     }
@@ -70,50 +73,50 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+        let actionNotifications = NSKeyedArchiver.archivedData(withRootObject: SettingsViewController.actionNotifications)
+        UserDefaults.standard.set(actionNotifications, forKey: "actionNotifications")
     }
     
     func application(_ app: UIApplication, open url: URL, options: [UIApplicationOpenURLOptionsKey : Any] = [:]) -> Bool {
         return SDKApplicationDelegate.shared.application(app, open: url, options: options)
     }
+    
+    func registerForPushNotifications() {
+        centerNotification.delegate = self
+        centerNotification.requestAuthorization(options: [.badge, .sound, .alert], completionHandler: {(granted, error) in
+            if (granted) {
+                UIApplication.shared.registerForRemoteNotifications()
+            } else{
+                print("Notification permissions not granted")
+            }
+        })
+    }
+    
+    func application(_ application: UIApplication,
+                     didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        let tokenParts = deviceToken.map { data -> String in
+            return String(format: "%02.2hhx", data)
+        }
+        
+        let token = tokenParts.joined()
+        print("Device Token: \(token)")
+    }
+    
+    func application(_ application: UIApplication,
+                     didFailToRegisterForRemoteNotificationsWithError error: Error) {
+        print("Failed to register: \(error)")
+    }
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
 
-  func registerForPushNotifications() {
-    UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound, .badge, .carPlay]) {
-      (granted, error) in
-      print("Permission granted: \(granted)")
-      guard granted else { return }
-      self.getNotificationSettings()
-    }
-  }
-  
-  func getNotificationSettings() {
-    UNUserNotificationCenter.current().getNotificationSettings { (settings) in
-      print("Notification settings: \(settings)")
-      guard settings.authorizationStatus == .authorized else { return }
-      UIApplication.shared.registerForRemoteNotifications()
-    }
-  }
-  
-  func application(_ application: UIApplication,
-                   didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
-    let tokenParts = deviceToken.map { data -> String in
-      return String(format: "%02.2hhx", data)
+        NotificationCenter.default.post(name: NSNotification.Name(rawValue: "RemoteNotificationHandler"), object: nil, userInfo: notification.request.content.userInfo)
     }
     
-    let token = tokenParts.joined()
-    print("Device Token: \(token)")
-  }
-  
-  func application(_ application: UIApplication,
-                   didFailToRegisterForRemoteNotificationsWithError error: Error) {
-    print("Failed to register: \(error)")
-  }
-  
-  func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable : Any], fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
     
-    let dict = userInfo["aps"] as! NSDictionary
-    let message = dict["alert"] as! String
-    print(message)
-  }
-  
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+
+        NotificationCenter.default.post(name: NSNotification.Name(rawValue: "RemoteNotificationHandler"), object: nil, userInfo: response.notification.request.content.userInfo)
+    }
+    
 }
 
--- Everest/Everest/Controllers/Settings/SettingsViewController.swift
+++ Everest/Everest/Controllers/Settings/SettingsViewController.swift
@@ -8,16 +8,85 @@
 
 import UIKit
 import SwiftyJSON
+import UserNotifications
 
-
-class SettingsViewController: UIViewController {
-    var moment: Moment!
-    var user: User!
-    var action: Action!
+class SettingsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var notificationsTableView: UITableView!
+    
+    let notificationCenter = UNUserNotificationCenter.current()
+    static var actionNotifications: [Act] = []
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(handleingMessageNotification(_:)), name: NSNotification.Name(rawValue: "RemoteNotificationHandler"), object: nil)
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        notificationsTableView.delegate = self
+        notificationsTableView.dataSource = self
     }
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+        
+        notificationCenter.getDeliveredNotifications() { notifications in
+            for notifcation in notifications {
+                
+            }
+            
+            //            self.notificationCenter.removeDeliveredNotifications(withIdentifiers: [])
+        }
+        notificationsTableView.reloadData()
+    }
+    
+    @IBAction func onAddBtn(_ sender: UIButton) {
+        MainManager.shared.createNewAction(id: SettingsViewController.actionNotifications[sender.tag].id, completion:{(error) in
+            let alertController = UIAlertController(title: "Added", message: "You can view this newly added action in your Profile view.",  preferredStyle: UIAlertControllerStyle.alert)
+            let okAction = UIAlertAction(title: "OK", style: .cancel, handler: { (action:UIAlertAction!) in
+            })
+            alertController.addAction(okAction)
+            // Present Alert
+            self.present(alertController, animated: true, completion:nil)
+        })
+        SettingsViewController.actionNotifications.remove(at: sender.tag)
+        notificationsTableView.reloadData()
+    }
+    
+    func handleingMessageNotification (_ notification: NSNotification) {
+        if let actDect = notification.userInfo {
+            SettingsViewController.actionNotifications.append(Act(id: actDect["id"] as! String, category: actDect["category"] as! String, title: actDect["title"] as! String, score: Int(actDect["score"] as! String)!) )
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "notificationCell", for: indexPath) as? NotificationCell else { return UITableViewCell() }
+        
+        cell.act = SettingsViewController.actionNotifications[indexPath.row]
+        cell.addBtn.tag = indexPath.row
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return SettingsViewController.actionNotifications.count
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let headerView = UITableViewHeaderFooterView(frame: CGRect(x: 0, y: 0, width: notificationsTableView.frame.width, height: 50))
+        headerView.contentView.backgroundColor = UIColor(colorLiteralRed: 0xd3/0xFF, green: 0x23/0xFF, blue: 0x23/0xFF, alpha: 0.5)
+        
+        return headerView
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return "title"//actionNotifications[section].title
+    }
 }
