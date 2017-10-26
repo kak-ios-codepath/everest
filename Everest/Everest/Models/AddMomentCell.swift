89b2e8bdade2989523996a0243c6d9c1e2cb9156
Kaushik <urskaush@gmail.com>
2017-10-25 17:32:27 -0700

    Adding moment  cell to the user profile screen.

---
+++ Everest/Everest/Models/AddMomentCell.swift
@@ -0,0 +1,42 @@
+//
+//  AddMomentCell.swift
+//  Everest
+//
+//  Created by Kaushik on 10/25/17.
+//  Copyright © 2017 Kavita Gaitonde. All rights reserved.
+//
+
+import UIKit
+@objc protocol AddMomentCellDelegate {
+    @objc optional func addMomentCell(cell: AddMomentCell, addNewMomentToAction: String)
+}
+class AddMomentCell: UITableViewCell {
+
+    weak var addMomentCellDelegate : AddMomentCellDelegate?
+
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        
+    }
+    var selectedActId: String?{
+        didSet{
+            
+        }
+    }
+    @IBAction func addNewMomentToAct(_ sender: Any) {
+        
+        if self.addMomentCellDelegate != nil {
+            self.addMomentCellDelegate?.addMomentCell!(cell: self, addNewMomentToAction: self.selectedActId!)
+        }
+        
+        
+    }
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
